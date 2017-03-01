8c81831d1ca5dba8aa3df37094a9c166b46e24fd
Julian Simioni <julian@mapzen.com>
2017-03-01 15:29:34 -0500

    Use notify_slack to print build start and end messages
    This includes printing the elapsed time for builds.

--- script/build_pelias.sh
+++ script/build_pelias.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -euo pipefail # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
+PATH="${PATH}:$DIR/../bin" # make it easier to run scripts
+
+# use a file to mark that a build has started
 touch /tmp/.pelias-build-started
 
-echo "building"
+start_time=$SECONDS
+notify_slack "a pelias build is starting"
+
+# do the build
 sleep 5
+
+# remove tracking file, signifying a successfully completed build
 rm /tmp/.pelias-build-started
+
+# report on success
+end_time=$SECONDS
+elapsed_time=$(( $end_time - $start_time ))
+friendly_elapsed_time=`echo $((elapsed_time/86400))" days "$(date -d "1970-01-01 + $elapsed_time seconds" "+%H hours %M minutes %S seconds")` # calculate elapsed time from http://unix.stackexchange.com/questions/27013/displaying-seconds-as-days-hours-mins-seconds
+notify_slack "a build has finished! time taken: $friendly_elapsed_time"
